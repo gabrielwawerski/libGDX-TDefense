461c1f0482178fe852f860eee92437da12e9f79a
masmix1 <4masmix1@gmail.com>
2018-10-30 03:01:38 +0100

    work in world renderer and controller

--- core/src/com/tdefense/world/WorldRenderer.java
+++ core/src/com/tdefense/world/WorldRenderer.java
@@ -1,4 +1,22 @@
 package com.tdefense.world;
 
-public class WorldRenderer {
+public class WorldRenderer implements Disposable {
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private WorldController worldController;
+    
+    public WorldRenderrer(WorldController worldController) {
+        this.worldController = worldController;
+    }
+    
+    private void initialize() {}
+    
+    public void render() {}
+    
+    public void resize(int width, int height) {}
+    
+    @Override
+    public void dispose() {}
+    
+   0
 }
--- core/src/com/tdefense/world/world_controller/WorldController.java
+++ core/src/com/tdefense/world/world_controller/WorldController.java
@@ -2,4 +2,10 @@ package com.tdefense.world.world_controller;
 
 public class WorldController {
     private static final String TAG = WorldController.class.getSimpleName();
+    
+    public WorldController() {}
+    
+    public void update(float deltaTime) {}
+    
+    
 }
