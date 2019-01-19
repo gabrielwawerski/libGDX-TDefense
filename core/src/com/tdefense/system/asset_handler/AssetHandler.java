d2ba843db0ea86b4cf813d96373be3de957c4bdd
MX <4masmix1@gmail.com>
2019-01-19 13:22:39 +0100

    Add non parameter loadAssets method which loads assets synchronously by default, rename package to asset_handler

--- core/src/com/tdefense/system/asset_handler/AssetHandler.java
+++ core/src/com/tdefense/system/asset_handler/AssetHandler.java
@@ -1,4 +1,4 @@
-package com.tdefense.system.asset_manager;
+package com.tdefense.system.asset_handler;
 
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
@@ -18,7 +18,18 @@ public final class AssetHandler extends AssetManager {
     }
 
     /**
-     *{@link AssetHandler} can stop all code execution to load assets synchronously (on the same thread). This ensures
+     * This method stops all code execution to load assets synchronously (on the same thread). This ensures you that
+     * all assets have been loaded.
+     */
+    public void loadAssets() {
+        instance.load("tile.atlas", TextureAtlas.class);
+        instance.load("player.png", Texture.class);
+        instance.load("enemy.png", Texture.class);
+        instance.finishLoading();
+    }
+
+    /**
+     * {@link AssetHandler} can stop all code execution to load assets synchronously (on the same thread). This ensures
      * you that all assets have been loaded.
      * @param synchronously {@code true}, for synchronous asset load
      */
